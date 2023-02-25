10834d9711ec845c3ae2add6acae0e98b55348ae
iEmanuelG <emanu6578@gmail.com>
2023-02-25 16:34:42 -0500

    Flip card
    Añadido flip card y stats de los pokemon

--- pokedex.js
+++ pokedex.js
@@ -39,6 +39,14 @@ function fetchPokemons(offset, limit) {
 }
 
 function createPokemon(pokemon) {
+  const flipCard = document.createElement("div");
+  flipCard.classList.add("flip-card");
+
+  const cardContainer = document.createElement("div");
+  cardContainer.classList.add("card-container");
+
+  flipCard.appendChild(cardContainer);
+
   const card = document.createElement("div");
   card.classList.add("pokemon-block");
 
@@ -61,8 +69,52 @@ function createPokemon(pokemon) {
   card.appendChild(number);
   card.appendChild(name);
 
-  pokemonContainer.appendChild(card);
+  const cardBack = document.createElement("div");
+  cardBack.classList.add("pokemon-block-back");
+
+
+  cardBack.appendChild(progressBars(pokemon.stats));
+  cardContainer.appendChild(card);
+  cardContainer.appendChild(cardBack);
+  pokemonContainer.appendChild(flipCard);
 }
+
+function progressBars(stats) {
+  const statsContainer = document.createElement("div");
+  statsContainer.classList.add("stats-container");
+
+  for (let i = 0; i < 3; i++) {
+    const stat = stats[i];
+
+    const statPercent = stat.base_state / 2 + "%";
+    const statContainer = document.createElement("div");
+    statContainer.classList.add("stat-container");
+
+    const statName = document.createElement("div");
+    statName.textContent = stat.stat.name;
+
+    const progress = document.createElement("div");
+    progress.classList.add("progress");
+
+    const progressBar = document.createElement("div");
+    progressBar.classList.add("progress-bar");
+    progressBar.setAttribute("aria-valuenow", stat.base_stat);
+    progressBar.setAttribute("aria-valuemin", 0);
+    progressBar.setAttribute("aria-valuemax", 255);
+    progressBar.style.width = statPercent;
+
+    progressBar.textContent = stat.base_stat;
+
+    progress.appendChild(progressBar);
+    statContainer.appendChild(statName);
+    statContainer.appendChild(progress);
+    statsContainer.appendChild(statContainer);
+  }
+
+  return statsContainer;
+
+}
+
 
 function removeChildNodes(parent) {
   while (parent.firstChild) {
