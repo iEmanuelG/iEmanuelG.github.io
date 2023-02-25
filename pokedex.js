2943d28031e89764db4c769be4a798ef23ac6a00
iEmanuelG <emanu6578@gmail.com>
2023-02-25 15:33:48 -0500

    Estilización pokedex
    Agregado un spinner y el css de la pagina pokedex

--- pokedex.js
+++ pokedex.js
@@ -1,43 +1,46 @@
-const pokemonContainer = document.querySelector('.pokemon-container')
+const pokemonContainer = document.querySelector(".pokemon-container");
+const spinner = document.querySelector("#spinner");
 
 function fetchPokemon(id) {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
-        .then(res => res.json())
-        .then(data => {
-            createPokemon(data);
-        });
+  fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+    .then((res) => res.json())
+    .then((data) => {
+      createPokemon(data);
+      spinner.style.display = "none";
+    });
 }
 
 function fetchPokemons(number) {
-    for (let i = 0; i <= number; i++) {
-        fetchPokemon(i)
-    }
+  spinner.style.display = "block";
+  for (let i = 1; i <= number; i++) {
+    fetchPokemon(i);
+  }
 }
 
 function createPokemon(pokemon) {
-    const card = document.createElement('div');
-    card.classList.add('pokemon-block');
+  const card = document.createElement("div");
+  card.classList.add("pokemon-block");
 
-    const spriteContainer = document.createElement('div');
-    spriteContainer.classList.add('img-container');
+  const spriteContainer = document.createElement("div");
+  spriteContainer.classList.add("img-container");
 
-    const sprite = document.createElement('img');
-    sprite.src = pokemon.sprites.front_default
+  const sprite = document.createElement("img");
+  sprite.src = pokemon.sprites.front_default;
 
-    spriteContainer.appendChild(sprite);
+  spriteContainer.appendChild(sprite);
 
-    const number = document.createElement('p');
-    number.textContent = `#${pokemon.id.toString().padStart(3, 0)}`;
+  const number = document.createElement("p");
+  number.textContent = `#${pokemon.id.toString().padStart(3, 0)}`;
 
-    const name = document.createElement('p');
-    name.classList.add('name');
-    name.textContent = pokemon.name
+  const name = document.createElement("p");
+  name.classList.add("name");
+  name.textContent = pokemon.name;
 
-    card.appendChild(spriteContainer);
-    card.appendChild(number);
-    card.appendChild(name);
+  card.appendChild(spriteContainer);
+  card.appendChild(number);
+  card.appendChild(name);
 
-    pokemonContainer.appendChild(card);
+  pokemonContainer.appendChild(card);
 }
 
 fetchPokemons(9);
